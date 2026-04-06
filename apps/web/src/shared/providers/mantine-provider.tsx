c23c92a6ed5672bb97609d2ed7309efbc66a1b9e
Abhinavpreet Singh Arora <aroraabhinavpreetsingh@gmail.com>
2026-04-06 20:59:27 +0530

    removed the imports of mantine to fix deployment build

--- apps/web/src/shared/providers/mantine-provider.tsx
+++ apps/web/src/shared/providers/mantine-provider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { MantineProvider as BaseMantineProvider } from "@mantine/core";
-import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import { theme } from "@/styles/theme";
 
@@ -9,7 +8,7 @@ export function MantineProvider({ children }: { children: React.ReactNode }) {
 	return (
 		<BaseMantineProvider theme={theme}>
 			<Notifications position="top-right" />
-			<ModalsProvider>{children}</ModalsProvider>
+			{children}
 		</BaseMantineProvider>
 	);
 }
